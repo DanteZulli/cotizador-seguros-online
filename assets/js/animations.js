2af0fa1a355977a6db197abcfc86efbf5ab0fe54
Dante Zulli <dantezulli2004@gmail.com>
2023-06-23 11:53:46 -0300

    responsive design, styles and animations <3

---
+++ assets/js/animations.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const textElement = document.querySelector("left-slide") + document.querySelector("right-slide") + document.querySelector("bottom-slide") + document.querySelector("top-slide");
+  textElement.classList.add("animated");
+});
